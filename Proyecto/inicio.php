2758aebb225bd1937134c89701616fea03283919
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-10-16 18:28:28 +0200

    sfeh

--- Proyecto/inicio.php
+++ Proyecto/inicio.php
@@ -19,9 +19,9 @@ $miCinta->mostrarCintaVideo();
 
 
 include "Dvd.php";
-echo "<h3>Prueba Dvd.php</h3>"
+echo "<h3>Prueba Dvd.php</h3>";
 $miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9"); 
-echo "<strong>" . $miDvd->titulo . "</strong>"; 
+echo "<strong>" . $miDvd->getTitulo() . "</strong>"; 
 echo "<br>Precio: " . $miDvd->getPrecio() . " euros"; 
 echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
 $miDvd->mostrarDvd();
